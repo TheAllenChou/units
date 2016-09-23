98beecf7b8b28469041e571eea10f90ee426192b
Allen Chou <cjcat2266@gmail.com>
2016-09-23 09:27:27 -0700

    Proper unit type name.

--- src/app/unit-test/unit-test.cpp
+++ src/app/unit-test/unit-test.cpp
@@ -14,29 +14,29 @@
 // Registers the fixture into the 'registry'
 CPPUNIT_TEST_SUITE_REGISTRATION(UnitTest);
 
-MAKE_BASIC_UNIT(Meter, float, _m);
-MAKE_BASIC_UNIT(Second, float, _s);
-MAKE_DERIVED_UNIT_DIV(MeterPerSecond, float, _m_s, Meter, Second);
+MAKE_BASIC_UNIT(Length, float, _m);                         // meter (m)
+MAKE_BASIC_UNIT(Time, float, _s);                           // second (s)
+MAKE_DERIVED_UNIT_DIV(Velocity, float, _m_s, Length, Time); // meters per second (m/s)
 
-MAKE_BASIC_UNIT(Ampere, float, _a);
-MAKE_BASIC_UNIT(Volt, float, _v);
-MAKE_DERIVED_UNIT_MUL(Watt, float, _w, Ampere, Volt);
+MAKE_BASIC_UNIT(Current, float, _a);                        // ampere (a)
+MAKE_BASIC_UNIT(Voltage, float, _v);                        // volt (v)
+MAKE_DERIVED_UNIT_MUL(Power, float, _w, Current, Voltage);  // watt (w)
 
 
 void UnitTest::TestBasicUnits()
 {
-  const Meter kPosInfinity = Meter::PosInfinity();
-  const Meter kNegInfinity = Meter::NegInfinity();
-  const Meter kZero = Meter::Zero();
+  const Length kPosInfinity = Length::PosInfinity();
+  const Length kNegInfinity = Length::NegInfinity();
+  const Length kZero = Length::Zero();
 
-  const Meter a = 1_m;
-  const Meter b = 2.0_m;
-  const Meter c = a + b;
-  const Meter d = 2 * b;
-  const Meter e = 4.0f * b;
-  const Meter f = b / 2;
-  Meter g = b; g *= 2;
-  Meter h = b; h /= 2.0f;
+  const Length a = 1_m;
+  const Length b = 2.0_m;
+  const Length c = a + b;
+  const Length d = 2 * b;
+  const Length e = 4.0f * b;
+  const Length f = b / 2;
+  Length g = b; g *= 2;
+  Length h = b; h /= 2.0f;
 
   CPPUNIT_ASSERT_EQUAL(FLT_MAX, kPosInfinity.To<float>());
   CPPUNIT_ASSERT_EQUAL(-FLT_MAX, kNegInfinity.To<float>());
@@ -62,20 +62,20 @@ void UnitTest::TestBasicUnits()
 
 void UnitTest::TestDerivedUnits()
 {
-  const Meter length = 4.0_m;
-  const Second time = 2.0_s;
-  const MeterPerSecond speed = length / time;
-  const Meter derivedLength = speed * 4.0_s;
+  const Length length = 4.0_m;
+  const Time time = 2.0_s;
+  const Velocity speed = length / time;
+  const Length derivedLength = speed * 4.0_s;
 
   CPPUNIT_ASSERT_EQUAL(2.0f, speed.GetRaw());
-  CPPUNIT_ASSERT(Meter::From(8.0f) == derivedLength);
+  CPPUNIT_ASSERT(Length::From(8.0f) == derivedLength);
 
-  const Ampere current = 2.0_a;
-  const Volt voltage = 3.0_v;
-  const Watt power0 = current * voltage;
-  const Watt power1 = voltage * current;
-  const Ampere derivedCurrent = power0 / voltage;
-  const Volt derivedVoltage = power1 / current;
+  const Current current = 2.0_a;
+  const Voltage voltage = 3.0_v;
+  const Power power0 = current * voltage;
+  const Power power1 = voltage * current;
+  const Current derivedCurrent = power0 / voltage;
+  const Voltage derivedVoltage = power1 / current;
 
   CPPUNIT_ASSERT_EQUAL(6.0f, power0.GetRaw());
   CPPUNIT_ASSERT(power0 == power1);
